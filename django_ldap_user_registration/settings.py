58d3fd6eb148523999ddd5713443d0bec061e359
Lee0701 <hgl1002@naver.com>
2023-07-17 01:50:01 +0900

    Add email settings from environments to the base settings file

--- django_ldap_user_registration/settings.py
+++ django_ldap_user_registration/settings.py
@@ -125,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = (os.environ.get('EMAIL_USE_TLS') or "false").lower() == 'true'
