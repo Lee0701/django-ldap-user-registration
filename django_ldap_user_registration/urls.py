848a51d1458856162c71f5c3ccad93bfd19a89f1
Lee0701 <hgl1002@naver.com>
2023-11-21 14:58:27 +0900

    Add i18n url prefix

--- django_ldap_user_registration/urls.py
+++ django_ldap_user_registration/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.utils.translation import gettext_lazy as _
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('user/', include('dl_user.urls')),
-]
+)
